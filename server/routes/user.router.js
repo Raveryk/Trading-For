7ceecef9bbdfa0122f1deb5375e55c2076d4957d
Raveryk <raveryk@gmail.com>
2021-05-24 14:53:50 -0500

    Modified Registration post route to include extra input fields.

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -20,11 +20,17 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 router.post('/register', (req, res, next) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
+  const email = req.body.email;
+  // console.log(req.body.email)
+  const phoneNum = req.body.phoneNum;
+  // console.log(req.body.phoneNum)
 
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+
+
+  const queryText = `INSERT INTO "user" (username, password, email, phone_num)
+    VALUES ($1, $2, $3, $4) RETURNING id`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [username, password, email, phoneNum])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField';
 
@@ -9,23 +10,37 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   form: {
-    alignItems: 'center',
     padding: '2%',
+    alignContent: 'center',
+    justifyContent: 'center',
   },
   btn: {
-    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+
   },
   formCard: {
-    justify: 'center'
+    justifyContent: 'center',
+    padding: '2%',
+
   }
 })
-
 
 function RegisterForm() {
 
   
+
+  
   const [username, setUsername] = useState('');
+  // console.log('username:', username)
   const [password, setPassword] = useState('');
+  // console.log('password:', password)
+  const [email, setEmail] = useState('');
+  // console.log('email:', email)
+  const [phoneNum, setPhoneNum] = useState('');
+  // console.log('phoneNum:', phoneNum)
+
+
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
@@ -39,14 +54,17 @@ function RegisterForm() {
       payload: {
         username: username,
         password: password,
+        email: email,
+        phoneNum: phoneNum
       },
     });
   }; // end registerUser
 
   return (
+    <Grid item xs={12}>
     <Card elevation={4}>
     <form className={classes.formCard} onSubmit={registerUser}>
-      <h2>Register User</h2>
+      <h2 >Register User</h2>
       {errors.registrationMessage && (
         <h3 className="alert" role="alert">
           {errors.registrationMessage}
@@ -75,10 +93,33 @@ function RegisterForm() {
           />
       </div>
       <div>
-        <Button className={classes.btn} type="submit" name="submit">Register</Button>
+          <TextField
+            className={classes.form}
+            placeholder="email"
+            type="text"
+            name="email"
+            value={email}
+            required
+            onChange={(event) => setEmail(event.target.value)}
+          />
+      </div>
+      <div>
+          <TextField
+            className={classes.form}
+            placeholder="phone #"
+            type="text"
+            name="phoneNum"
+            value={phoneNum}
+            required
+            onChange={(event) => setPhoneNum(event.target.value)}
+          />
+      </div>
+      <div className={classes.btn}>
+        <Button  type="submit" name="submit">Register</Button>
       </div>
     </form>
   </Card>
+  </Grid>
   );
 }
 
