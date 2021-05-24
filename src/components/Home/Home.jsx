54bb1db129a8211a1ced34b493c645f18a69023a
Raveryk <raveryk@gmail.com>
2021-05-24 18:00:46 -0500

    Created dispatch in Home component - need to troubleshoot.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,22 @@ import React, { useState } from 'react';
 import List from '@material-ui/core/List';
 
 import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch} from 'react-redux';
+import { useEffect } from 'react';
 
 // CUSTOM COMPONENTS
 
 function Home() {
+
+  const dispatch = useDispatch();
   const [heading, setHeading] = useState('Welcome');
+
+  useEffect(() => (
+    dispatch({ type:'FETCH_POSTS'})
+  ), [])
+
+  const posts = useSelector( (store) => store.posts )
+  console.log(posts)
 
   
 
