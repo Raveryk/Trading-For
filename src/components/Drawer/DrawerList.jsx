0ad64fc8fdeb528088d7c10700dadefe5c7779f8
Raveryk <raveryk@gmail.com>
2021-05-25 14:27:23 -0500

    Created links within nav bar and added welcome. Need to align with wireframe.

--- src/components/Drawer/DrawerList.jsx
+++ src/components/Drawer/DrawerList.jsx
@@ -1,62 +1,76 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useSelector } from "react-redux";
 
-import {List, ListItem} from '@material-ui/core';
+import "../Nav/Nav.css";
 
+import { List, ListItem, Typography} from "@material-ui/core";
 
 function DrawerList() {
-
-    const user = useSelector((store) => store.user);
+  const user = useSelector((store) => store.user);
+  console.log(user);
 
   let loginLinkData = {
-    path: '/login',
-    text: 'Login / Register',
+    path: "/login",
+    text: "Login / Register",
   };
 
   if (user.id != null) {
-    loginLinkData.path = '/user';
-    loginLinkData.text = 'Home';
+    loginLinkData.path = "/user";
+    loginLinkData.text = "Home";
   }
 
-    return(
+  return (
     <div>
+      
       <List>
         <ListItem>
-    <Link to="/home">
-        <h2 className="nav-title">Trading For</h2>
-      </Link>
-      </ListItem>
-      <div>
-      <ListItem>
-        <Link className="navLink" to={loginLinkData.path}>
-          {loginLinkData.text}
-        </Link>
+          <Link to="/home">
+            <h2 className="nav-title">Trading For</h2>
+          </Link>
         </ListItem>
-      </div>
-      <div>
-      <ListItem>
-        {user.id && (
-          <>
-            <Link className="navLink" to="/info">
-              Info Page
+        <Typography>
+        Welcome back, {user.username}!
+      </Typography>
+        <div>
+          <ListItem>
+            <Link className="navLink" to={loginLinkData.path}>
+              {loginLinkData.text}
             </Link>
-            <LogOutButton className="navLink" />
-          </>
-        )}
-      </ListItem>
+          </ListItem>
         </div>
         <div>
-        <ListItem>
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-        </ListItem>
+          <ListItem>
+            {user.id && (
+              <>
+                <Link className="navLink" to="/info">
+                  Info Page
+                </Link>
+              </>
+            )}
+          </ListItem>
         </div>
-        </List>
+        <div>
+          <ListItem>
+            <Link className="navLink" to="/about">
+              About
+            </Link>
+          </ListItem>
         </div>
-    )
+        <div>
+          <ListItem>
+            {user.id && (
+              <>
+                <LogOutButton className="navLink" />
+              </>
+            )}
+          </ListItem>
+        </div>
+        
+      </List>
+    </div>
+  );
 }
 
 export default DrawerList;
