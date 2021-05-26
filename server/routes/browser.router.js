6dca42d183480bb5055fea031b61c4fe0c5a3c64
Raveryk <raveryk@gmail.com>
2021-05-26 10:38:03 -0500

    Added GET request for details in router.

--- server/routes/browser.router.js
+++ server/routes/browser.router.js
@@ -2,8 +2,8 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+// GET request to grab all posts that haven't been traded
 router.get('/', (req, res) => {
-    // GET route code here
     const query = `SELECT * FROM posts 
     JOIN "user" ON "user".id=posts.users_id
     WHERE traded=false;`;
@@ -14,6 +14,26 @@ router.get('/', (req, res) => {
       })
       .catch( error => {
         console.log('Something went wrong GETting posts for browser:', error)
+        res.sendStatus(500);
+      })
+  });
+
+  // GET request for specific post id
+  router.get(`/details/:id`, (req, res) => {
+      console.log(req.params.id);
+      postId = req.params.id
+    // GET route code here
+    const query = `SELECT * FROM posts 
+    JOIN "user" ON "user".id=posts.users_id
+    WHERE user_id=$1;`;
+
+    pool.query(query, [postId])
+      .then(result => {
+        console.log(result.rows);
+        res.send(result.rows)
+      })
+      .catch( error => {
+        console.log('Something went wrong GETting details:', error)
         res.sendStatus(500);
       })
   });
